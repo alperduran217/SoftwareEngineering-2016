e7733541c7aa94b75723033a636e501935e4dbaa
alperduran217 <alperduran217@gmail.com>
2016-03-16 00:33:23 +0200

    Working on Moment

--- Civil/MomentViewController.swift
+++ Civil/MomentViewController.swift
@@ -27,14 +27,70 @@ class MomentViewController: UIViewController, UIScrollViewDelegate {
     }
 
     
+    // Variables
+    
+     var bigArray:[[Double]] =  [[Double]]()
+    
+    var row = 0
+    var column = 0
+    
     // Main.storyboard Section
 
+    @IBOutlet weak var xField: UITextField!
+    
+    @IBOutlet weak var yField: UITextField!
+    
+    @IBOutlet weak var zField: UITextField!
+    
+    @IBOutlet weak var iField: UITextField!
+    
+    @IBOutlet weak var jField: UITextField!
+    
+    @IBOutlet weak var kField: UITextField!
     
     @IBOutlet weak var pageControl: UIPageControl!
 
     @IBOutlet weak var scrollView: UIScrollView!
     
     // Coding Section
+    @IBAction func addition(sender: AnyObject) {
+        
+        if column == 6 {
+            
+            column = 0
+        }
+        
+        bigArray[row].append(Double(iField.text!)!)
+        column = column + 1
+        
+        bigArray[row].append(Double(jField.text!)!)
+        column = column + 1
+        
+        bigArray[row].append(Double(kField.text!)!)
+        column = column + 1
+        
+        bigArray[row].append(Double(xField.text!)!)
+        column = column + 1
+        
+        bigArray[row].append(Double(yField.text!)!)
+        column = column + 1
+        
+        bigArray[row].append(Double(zField.text!)!)
+        column = column + 1
+        row = row + 1
+        
+        print(bigArray)
+        
+        iField.text = nil
+        jField.text = nil
+        kField.text = nil
+        xField.text = nil
+        yField.text = nil
+        zField.text = nil
+        
+    }
+    
+
     
     func scrollViewDidEndDecelerating(scrollView: UIScrollView){
         
